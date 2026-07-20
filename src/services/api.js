8aee38719d3cc345c226ca4ecff44f289d51134d
firdowshieceimps-png <firdowshi.ece.imps@gmail.com>
2026-07-20 13:58:15 +0000

    Update api.js
    add base url

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
+const API_BASE_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+
 const api = axios.create({
-
-  baseURL: "http://localhost:5000/api",
-
+  baseURL: API_BASE_URL,
 });
 
 api.interceptors.request.use((config) => {
@@ -11,9 +12,7 @@ api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
   if (token) {
-
     config.headers.Authorization = `Bearer ${token}`;
-
   }
 
   return config;
@@ -21,4 +20,3 @@ api.interceptors.request.use((config) => {
 });
 
 export default api;
-
